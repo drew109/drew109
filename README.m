edf91350e4c6898d48f16a69e953990dd32e0bdf
ykyk21222 <88405575+ykyk21222@users.noreply.github.com>
2022-03-08 16:28:11 -0500

    first commit

---
+++ README.m
@@ -0,0 +1,61 @@
+[comment]: <> (Credits to https://github.com/codeSTACKr I give my credits im not a skid)
+[comment]: <> (Credits to my friend https://github.com/Ethan-Francolla/Ethan-Francolla)
+
+### Hey Im Neon, and I'm a highschool student in the U.S!!👋
+<hr style="border:2px solid gray"> </hr>
+
+<!--- pfp --->
+<img 
+     align="left" alt="Avatar" width="200px" 
+          src="https://avatars.githubusercontent.com/u/29808983?s=400&u=ceb9e5572367bd7e323b60313c61ce6238e14ce7&v=4" 
+/>
+
+<!--- Info about me --->
+### I'm a Student and Developer!
+- 📚 I’m currently learning JS and PY
+- ✔️ 2022 Goals: Learn more about JS
+- ⚡ Fun fact: I love Floppa 😻
+
+<!--- Line  --->
+<hr style="border:2px solid gray"> </hr>
+
+<!--- My knowledge  --->
+### Languages and Tools:
+
+<img align="left" alt="HTML5" width="26px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" style="padding-right:10px;" />
+<img align="left" alt="CSS3" width="26px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" style="padding-right:10px;" />
+<img align="left" alt="JavaScript" width="26px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" style="padding-right:10px;"/>
+<img align="left" alt="Python" width="26px" src="https://brandslogos.com/wp-content/uploads/images/large/python-logo.png" style="padding-right:10px;" />
+<img align="left" alt="Node.js" width="26px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" style="padding-right:10px;" />
+<img align="left" alt="MySQL" width="26px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" style="padding-right:10px;" />
+<img align="left" alt="Git" width="26px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" style="padding-right:10px;" />
+<img align="left" alt="Visual Studio Code" width="26px" src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" style="padding-right:10px;"/>
+<img align="left" alt="PyCharm" width="26px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/PyCharm_Icon.svg/2048px-PyCharm_Icon.svg.png" style="padding-right:10px;"/>
+
+<!--- Line+placeholder  --->
+&ensp;
+<hr style="border:1px solid gray"> </hr>
+
+👀 Latest Activities
+
+<!-- Projects -->
+- [Portfolio🗒️](https://github.com/drew109/Portfolio)
+- [Discord Bot aka Monke Bot🐒](https://github.com/drew109/Monke-Discord-Bot)
+- 
+- 
+- 
+
+
+<!-- BLOG-POST-LIST:START -->
+
+<!-- BLOG-POST-LIST:END -->
+
+
+
+
+<details>
+  <summary>:zap: GitHub Stats</summary>
+
+  <img align="left" alt="Neon's GitHub Stats" src="https://github-readme-stats.vercel.app/api?username=codeSTACKr&show_icons=true&hide_border=false&title_color=ff652f&icon_color=FFE400&bg_color=09131B&text_color=ffffff&border_color=0c1a25" />
+
+</details>
